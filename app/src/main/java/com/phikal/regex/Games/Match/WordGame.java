952e9053c31b667b0940f79e82bac8556662f129
Philip K <philip@warpmail.net>
2018-07-29 23:46:15 +0200

    Rewrote WordGame and removed external source support

--- app/src/main/java/com/phikal/regex/Games/Match/WordGame.java
+++ app/src/main/java/com/phikal/regex/Games/Match/WordGame.java
@@ -1,44 +1,41 @@
 package com.phikal.regex.Games.Match;
 
-import android.app.Activity;
 import android.content.Context;
 
-import com.phikal.regex.Games.TaskGenerationException;
-import com.phikal.regex.Utils.Task;
-import com.phikal.regex.Utils.Word;
-import com.phikal.regex.Utils.WordList;
+import com.phikal.regex.Models.Progress;
+import com.phikal.regex.Models.Task;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.zip.GZIPInputStream;
 
 public class WordGame extends SimpleMatchGame {
 
-    final Context ctx;
-    final WordList wlist;
+    final private List<String> words;
+    private int request;
 
-    public WordGame(final Activity activity) {
-        ctx = activity;
-        wlist = new WordList(activity);
+    public WordGame(Context ctx, Progress p) throws IOException {
+        super(ctx, p);
+        words = new ArrayList<>(8711); // current length
+        BufferedReader bis = new BufferedReader( new InputStreamReader( new GZIPInputStream(
+                ctx.getAssets() .open("words.gz"))));
+        for (String line; (line = bis.readLine()) != null; )
+            words.add(line);
     }
 
     @Override
-    public int calcMax(Task t, int lvl) {
-        return (int) Math.ceil(Math.sqrt(super.calcMax(t, lvl)) * 1.75);
+    public synchronized Task nextTask() {
+        request = 0;
+        Collections.shuffle(words, rnd);
+        return super.nextTask();
     }
 
     @Override
-    public Task genTask(int lvl) throws TaskGenerationException {
-        lvl += 5;
-        int tma = Calc.calcRWLCount(lvl, 1.75),
-                dma = Calc.calcRWLCount(lvl, 1.25),
-                len = Calc.calcRWLen(lvl);
-        List<Word> words = new ArrayList<>(tma + dma);
-        for (String s : wlist.rndWord(len, tma + dma))
-            words.add(new Word(s.trim()));
-        try {
-            return new Task(words.subList(0, tma), words.subList(tma + 1, tma + dma), null);
-        } catch (IndexOutOfBoundsException iae) {
-            return genTask(lvl + 2); // retry with higher level
-        }
+    String randString() {
+        return words.get(request++);
     }
 }
