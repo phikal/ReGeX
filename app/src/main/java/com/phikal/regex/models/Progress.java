dbeed457bd5b1a4cd61ed5853722b9c6a5b15aec
Philip K <philip@warpmail.net>
2018-08-03 17:35:32 +0200

    Reworked Progress class

--- app/src/main/java/com/phikal/regex/models/Progress.java
+++ app/src/main/java/com/phikal/regex/models/Progress.java
@@ -3,39 +3,42 @@ package com.phikal.regex.models;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import static com.phikal.regex.Util.COUNT;
 import static com.phikal.regex.Util.PROGRESS;
 
 public class Progress {
 
-    private static final int MAX_TASKS = 12;
-    private static final double A = 1, Q = 1 / 2;
+    private static final float Q = 0.01f;
 
     private String name;
-    private Context ctx;
+    private transient Context ctx;
     private double difficulty;
     private int rounds;
 
     public Progress(Context ctx, String name) {
         SharedPreferences pm = PreferenceManager.getDefaultSharedPreferences(ctx);
 
-        this.difficulty = pm.getFloat(name + PROGRESS, 0f);
+        this.difficulty = pm.getFloat(name + PROGRESS, Q);
         this.rounds = pm.getInt(name + COUNT, 1);
 
         this.ctx = ctx;
         this.name = name;
+        Log.d("progress", name + ": " + difficulty + "/" + rounds);
     }
 
-    public Progress(Context ctx, String name, Progress p) {
-        this.difficulty = p.getDifficutly() + A * Math.pow(Q, p.getRound());
-        this.rounds = 1;
-        this.ctx = ctx;
+    private Progress(String name, Context ctx, double difficulty, int rounds) {
         this.name = name;
+        this.ctx = ctx;
+        this.difficulty = difficulty;
+        this.rounds = rounds;
     }
 
-    public int getMaxTasks() {
-        return (int) (Math.pow(difficulty, 2) * MAX_TASKS);
+    public Progress next(double factor) {
+        return new Progress(name, ctx,
+                difficulty + factor * Q * Math.pow(1 / (Q + 1), rounds),
+                rounds + 1);
     }
 
     public double getDifficutly() {
